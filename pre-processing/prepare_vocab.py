341eb2112faf692dbff81f6f49297167e154057d
Daniel <daniel@mail.de>
2020-02-05 18:20:47 +0100

    Refactor script.

--- pre-processing/prepare_vocab.py
+++ pre-processing/prepare_vocab.py
@@ -1,55 +1,49 @@
-#! /usr/bin/env python
-
-"""
-1. Load text corpus
-2. Clean text
-3. Extend with transcriptions from training data
-4. Save
-"""
-
-import os
-import sys
-
-sys.path.append(os.path.abspath(os.path.join(__file__, os.path.pardir)))
-
 import argparse
-from audiomate.utils import textfile
 
 import text_cleaning
+import tqdm
+from audiomate.utils import textfile
 
+
+# ======================================================================================================================
 
 def read_training_transcripts(path):
     transcripts = []
+    lines = textfile.read_separated_lines_generator(path, separator=',', max_columns=3,
+                                                    ignore_lines_starting_with=['wav_filename'])
 
-    for entry in textfile.read_separated_lines_generator(path, separator=',', max_columns=3,
-                                                         ignore_lines_starting_with=['wav_filename']):
+    for entry in tqdm.tqdm(list(lines)):
         transcripts.append(entry[2])
 
     return transcripts
 
 
+# ======================================================================================================================
+
 parser = argparse.ArgumentParser(description='Clean text corpus.')
 parser.add_argument('source_path', type=str)
 parser.add_argument('target_path', type=str)
 parser.add_argument('--training_csv', type=str)
-
 args = parser.parse_args()
 
-index = 0
+cleaned_sentences = []
 
-with open(args.source_path, 'r', encoding="utf-8") as source_file, open(args.target_path, 'w',
-                                                                        encoding="utf-8") as target_file:
-    for index, line in enumerate(source_file):
-        cleaned_sentence = text_cleaning.clean_sentence(line)
-        target_file.write('{}\n'.format(cleaned_sentence))
+# Load text corpus
+with open(args.source_path, 'r', encoding="utf-8") as source_file:
+    text_data = source_file.readlines()
+    text_data = [x.strip() for x in text_data]
 
-        if index % 1000 == 0:
-            print(index)
+print("Adding sentences from source file")
+for line in tqdm.tqdm(text_data):
+    cleaned_sentence = text_cleaning.clean_sentence(line)
+    cleaned_sentences.append(cleaned_sentence)
 
-    print('Cleaned {} lines!'.format(index))
+if args.training_csv is not None:
+    print("Adding transcripts from training data")
+    training_transcripts = read_training_transcripts(args.training_csv)
+    cleaned_sentences.extend(training_transcripts)
 
-    if args.training_csv is not None:
-        training_transcripts = read_training_transcripts(args.training_csv)
-        target_file.write('\n'.join(training_transcripts))
-
-        print('Added {} transcripts from training data!'.format(len(training_transcripts)))
+# Save data
+with open(args.target_path, 'a', encoding="utf-8") as target_file:
+    text = '\n'.join(cleaned_sentences)
+    target_file.write(text)
