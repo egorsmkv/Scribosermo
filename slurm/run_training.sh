c444cd56a415d727b498f516f9c151cf658a0f39
Daniel <daniel@mail.de>
2020-04-08 14:31:51 +0200

    Update slurm container and config.

--- slurm/run_training.sh
+++ slurm/run_training.sh
@@ -7,13 +7,13 @@
 #SBATCH --mem 262144
 #SBATCH --time=1000:00:00 # Time after which the job will be aborted
 #
+#
 # Actual singularity call with nvidia capabilities, amounted folder and call to script
-singularity exec --nv \
-  -B /cfs/share/cache/db_xds/checkpoints/:/DeepSpeech/checkpoints/ \
-  -B /cfs/share/cache/db_xds/data_original/:/DeepSpeech/data_original/ \
-  -B /cfs/share/cache/db_xds/data_prepared/:/DeepSpeech/data_prepared/ \
-  -B /cfs/share/cache/db_xds/deepspeech-german/:/DeepSpeech/deepspeech-german/ \
-  -B /cfs/share/cache/db_xds/DeepSpeech/evaluate.py:/DeepSpeech/evaluate.py \
-  -B /cfs/share/cache/db_xds/DeepSpeech/DeepSpeech.py:/DeepSpeech/DeepSpeech.py \
-  /cfs/share/cache/db_xds/images/deep_speech_german.sif \
-  /bin/bash deepspeech-german/training/train.sh checkpoints/voxforge/ data_prepared/voxforge/train_azce.csv data_prepared/voxforge/dev_azce.csv data_prepared/voxforge/test_azce.csv 1 --
+singularity exec \
+  --nv \
+  --bind ~/checkpoints/:/DeepSpeech/checkpoints/ \
+  --bind /cfs/share/cache/db_xds/data_original/:/DeepSpeech/data_original/ \
+  --bind /cfs/share/cache/db_xds/data_prepared/:/DeepSpeech/data_prepared/ \
+  --bind ~/deepspeech-german/:/DeepSpeech/deepspeech-german/ \
+  /cfs/share/cache/db_xds/images/mds_slurm.sif \
+  /bin/bash /DeepSpeech/deepspeech-german/training/train.sh /DeepSpeech/checkpoints/voxforge/ /DeepSpeech/data_prepared/voxforge/train_azce.csv /DeepSpeech/data_prepared/voxforge/dev_azce.csv /DeepSpeech/data_prepared/voxforge/test_azce.csv 1 --
