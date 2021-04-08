bf4d7d48cf5c407f2262f10ceaed8327120164a8
Daniel <daniel@mail.de>
2021-04-08 16:36:51 +0200

    Solve new warnings. Restricts testing to single gpu.

--- training/scode/testing.py
+++ training/scode/testing.py
@@ -10,6 +10,7 @@ from . import pipeline, training, utils
 
 # ==================================================================================================
 
+run_on_gpu_id = 0
 config = utils.get_config()
 checkpoint_dir = config["checkpoint_dir"]
 model: tf.keras.Model
@@ -193,7 +194,7 @@ def run_test(dataset_test):
         step += 1
 
         if log_greedy_steps != 0 and step % log_greedy_steps == 0:
-            training.log_greedy_text(samples)
+            training.log_greedy_text(samples, trainmode=False)
 
         results = get_texts(predictions, samples)
         test_results.extend(results)
@@ -211,16 +212,13 @@ def main():
     print("Starting test with config:")
     print(json.dumps(config, indent=2))
 
-    # Allow growing gpu memory
-    gpus = tf.config.experimental.list_physical_devices("GPU")
-    for gpu in gpus:
-        tf.config.experimental.set_memory_growth(gpu, True)
+    # Hide all but one gpus
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(run_on_gpu_id)
 
-    # Enable testing with multiple gpus
-    mirrored_strategy = tf.distribute.MirroredStrategy()
-    global_batch_size = (
-        config["batch_sizes"]["test"] * mirrored_strategy.num_replicas_in_sync
-    )
+    # Allow growing gpu memory on first gpu
+    gpus = tf.config.experimental.list_physical_devices("GPU")
+    if len(gpus) > 0:
+        tf.config.experimental.set_memory_growth(gpus[0], True)
 
     # Use exported config to set up the pipeline
     path = os.path.join(checkpoint_dir, "config_export.json")
@@ -228,30 +226,17 @@ def main():
 
     dataset_test = pipeline.create_pipeline(
         csv_path=config["data_paths"]["test"],
-        batch_size=global_batch_size,
+        batch_size=config["batch_sizes"]["test"],
         config=exported_config,
         train_mode=False,
     )
 
-    # dataset_test = mirrored_strategy.experimental_distribute_dataset(dataset_test)
-
-    # # Disable AutoShard
-    # options = tf.data.Options()
-    # options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.OFF
-    # dataset_test = dataset_test.with_options(options)
-
-    # options = tf.data.Options()
-    # options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.DATA
-    # dataset_test = dataset_test.with_options(options)
-
-    # Build model and print network summary
-    with mirrored_strategy.scope():
-        model = tf.keras.models.load_model(checkpoint_dir)
-        feature_type = exported_config["audio_features"]["use_type"]
-        c_input = exported_config["audio_features"][feature_type]["num_features"]
-        model.build(input_shape=(None, None, c_input))
-        model.compile()
-        model.summary()
+    model = tf.keras.models.load_model(checkpoint_dir, compile=False)
+    feature_type = exported_config["audio_features"]["use_type"]
+    c_input = exported_config["audio_features"][feature_type]["num_features"]
+    model.build(input_shape=(None, None, c_input))
+    model.compile()
+    model.summary()
 
     training.model = model
     training.create_idx2char()
